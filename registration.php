9814c4efa9fefb523a5c0c7ffaa9c65376b12be3
Artur Moczulski <artur.moczulski@gmail.com>
2018-07-12 22:40:21 -0400

    #2: remove experimental code

--- registration.php
+++ registration.php
@@ -3,13 +3,3 @@
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Rollbar_Magento2', __DIR__);
-
-// $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-// $deploymentConfig = $this->objectManager->get(\Magento\Framework\App\DeploymentConfig::class); 
-// $rollbarConfig = $this->deploymentConfig->get('rollbar/access_token');
-
-// var_dump($rollbarConfig); die();
-
-// \Rollbar\Rollbar::init($rollbarConfig);
-
-// \Rollbar\Rollbar::debug('This is a debug message from Magento 2');
