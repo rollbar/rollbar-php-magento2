4cc6f5230712adc1c773ac27c10881dff75504a0
Artur Moczulski <artur.moczulski@gmail.com>
2018-07-12 21:33:36 -0400

    #2: init rollbar before launch of the Magento2 app; read config from app/env.php

---
+++ Plugin/InitPlugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rollbar\Magento2\Plugin;
+
+class InitPlugin
+{
+	public function beforeLaunch(\Magento\Framework\AppInterface $subject)
+	{
+		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+		$deploymentConfig = $objectManager->get(\Magento\Framework\App\DeploymentConfig::class); 
+		$rollbarConfig = $deploymentConfig->get('rollbar');
+
+		\Rollbar\Rollbar::init($rollbarConfig);
+	}
+}
